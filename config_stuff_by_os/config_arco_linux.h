76808460f22143899e26992934dc9041fe1f0ad4
thinkpad in arco linux <sunyta2@naver.com>
2019-10-30 21:06:57 +0900

    add OS stuff from config_github Folder

--- config_stuff_by_os/config_arco_linux.h
+++ config_stuff_by_os/config_arco_linux.h
@@ -1,7 +1,6 @@
 /* See LICENSE file for copyright and license details. */
 /* (find-file "~/suckless/dwm/config.h") (shell-command "cp ~/suckless/dwm/config.h ~/config_github/app/dwm/config.h_arco_linux") */
 /* (find-file "~/config_github/app/dwm/config.h_arco_linux") (shell-command "cp ~/config_github/app/dwm/config.h_arco_linux ~/suckless/dwm/config.h") */
-/* shell_script/ 참조할 것. (shell-command "ln ~/config_github/app/dwm/config.h_arco_linux ~/suckless/dwm/config.h") 이것을 실행하므로서 하드링크로 sync하는 것이 가능하기를 기대한다. */
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
@@ -74,8 +73,7 @@ static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",	tile },    /* first entry is default */
 	{ "[M]",	monocle },
- 	//{ "[@]",	spiral }, /*둘 중에 한가지를 선택함 select in */
-  { "[@]",	dwindle }, /* dwindle을 나는 선택함 */
+ 	{ "[@]",	spiral },
 	{ "|M|",	centeredmaster },
 	{ "><>",	NULL },    /* no layout function means floating behavior */
 	{ ">M>",	centeredfloatingmaster },
@@ -95,16 +93,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *roficmd[] = { "rofi", "-show", "combi", NULL }; // ~/.config/rofi/config 에 설정을 가지고 있다.
-/* static const char *roficmd[] = { "rofi", "-combi", "window,run", "-show", "combi", NULL }; */
-//static const char *terminal_dwm[] = { "st", NULL }; // 투명한 기능에 st 매력적이다. xcompmgr compmgr 을 설치후 st로 변경함
-static const char *terminal_cmd[] = { "urxvt", NULL }; // 투명한 기능에 매력적이다.
-//static const char *fm_dwm[] = { "st", "-T", "emacsQ", "-e", "$HOME/.bin/emacsQ", NULL }; // 투명한 emacsQ 일단 File Manager.
-static const char *fm_dwm[] = { "urxvt", "-e", "sh", "-c", "$HOME/.bin/emacsQ", NULL }; // 투명한 emacsQ 일단 File Manager.
-//static const char *urxvt_fmcmd[] = { "urxvt", "-e", "sh", "-c", "$HOME/.bin/emacsQ", NULL }; // 투명한 emacsQ 일단 File Manager.
-//static const char *urxvt_fmcmd[] = { "urxvt", "-e", "sh", "-c", "ranger", NULL }; // 투명한 ranger 일단 File Manager.
-//static const char *urxvt_fmcmd[] = { "urxvt", "-e", "sh", "-c", "mc" NULL }; // 투명한 mc 대안 File Manager.
-// 대안의 항목 dolphin, spacefm, worker, mc, ranger, lscd, hunter ...
+static const char *roficmd[] = { "rofi", "-combi", "window,run", "-show", "combi", NULL };
 
 #include "movestack.c"
 static Key keys[] = {
@@ -126,12 +115,7 @@ static Key keys[] = {
 	{ MODKEY,			XK_u,		setlayout,	{.v = &layouts[3]} },
 	{ MODKEY,			XK_i,		setlayout,	{.v = &layouts[4]} },
 	{ MODKEY,			XK_o,		setlayout,	{.v = &layouts[5]} },
-	//{ MODKEY,			XK_Tab,		view,		{0} }, /*macosx와 유사한 Tab이동을 만들기전의 원본 아래의 4줄이 대신 추가됨*/
-	//{ MODKEY,			XK_~,		shiftview,	{ .i = 1 } }, /* 탭의 위의 키는 tag가 전진함.*/
-	//{ MODKEY,			XK_`,		view,		{0} }, /* 탭의 위의 키 + Shift키는 이전의 tag로 전환함.*/
-	{ MODKEY,			XK_Tab,		focusstack,	{.i = +1 } }, /* Mac의 CMD+Tab과 동일한 기능*/
-	//{ MODKEY|ShiftMask,		XK_Tab,		focusstack,	{.i = -1 } }, /* Mac의 CMD+Shift+Tab과 동일한 기능*/ /*사용되지만 일단 임시로 다른 것으로 전환*/
-	{ MODKEY|ShiftMask,		XK_Tab,		shiftview,	{ .i = 1 } }, /* 임시로 이전탭으로 전환하는 것으로 일단 사용함.*/ /*사용되지만 일단 임시로 다른 것으로 전환*/
+	{ MODKEY,			XK_Tab,		view,		{0} },
 	{ MODKEY,			XK_backslash,	view,		{0} },
 	{ MODKEY|ShiftMask,		XK_space,	togglefloating,	{0} },
 	/* { MODKEY,                       XK_space,  setlayout,      {0} }, */
@@ -154,11 +138,9 @@ static Key keys[] = {
 	{ MODKEY,			XK_g,		shiftview,	{ .i = -1 } },
 	{ MODKEY,			XK_semicolon,	shiftview,	{ .i = 1 } },
 	{ MODKEY,			XK_Page_Up,	shiftview,	{ .i = -1 } },
-	{ MODKEY,			XK_d,		spawn,		{ .v = dmenucmd } },
-	{ MODKEY,			XK_r,		spawn,		{ .v = roficmd } },
-  { MODKEY,         			XK_Return,		spawn,		{ .v = terminal_dwm } },    /* 투명터미널 실행을 목적 */
-  { MODKEY|ShiftMask,			XK_Return,		spawn,		{ .v = fm_dwm } }, /* 투명파일매니져 열어냄 */
-	{ MODKEY,			XK_Page_Down,	shiftview,	{ .i = 1 } },
+	{ MODKEY,			XK_d,	spawn,	{ .v = dmenucmd } },
+  { MODKEY,			XK_r,	spawn,	{ .v = roficmd } },
+  { MODKEY,			XK_Page_Down,	shiftview,	{ .i = 1 } },
 };
 
 /* button definitions */
